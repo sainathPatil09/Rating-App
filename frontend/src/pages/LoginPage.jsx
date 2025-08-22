2905c230ea32c7262a9397974215599840ce46de
sainathPatil09 <sainathpatilpanda@gmail.com>
2025-08-22 16:36:08 +0530

    images added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,37 +1,3 @@
-// import { useState, useContext } from "react";
-// import API from "../services/api";
-// import { AuthContext } from "../context/AuthContext";
-
-// const LoginPage = () => {
-//   const { login } = useContext(AuthContext);
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const { data } = await API.post("/auth/login", { email, password });
-//       login(data.user, data.token);
-//       alert("Login successful.");
-//     } catch (err) {
-//       alert(err.response?.data?.msg || "Login failed");
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <form onSubmit={handleSubmit}>
-//         <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-//         <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-//         <button type="submit">Login</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default LoginPage;
-
 import { useState, useContext } from "react";
 import { Store, Mail, Lock, Eye, EyeOff, ArrowLeft } from "lucide-react";
 import API from "../services/api";
---
+++ readme.md
@@ -0,0 +1,10 @@
+## IMAGES
+
+![App Screenshot](images/signup.png)
+![Logo](images/login.png)
+![Logo](images/user-dashboard.png)
+![Logo](images/profile.png)
+![Logo](images/homepage.png)
+![Logo](images/admin-dashboard.png)
+![Logo](images/owner-dashboard.png)
+![Logo](images/owner-dashboard2.png)
